e9ae187865ec6df74fa29c44ae4cc0ab00f3c4b6
HannahSav <g.savon11@yandex.ru>
2024-05-16 15:00:05 +0300

    dosen't workable but all pairs

--- counter.cpp
+++ counter.cpp
@@ -14,14 +14,13 @@
 #define k_b 8.31 * 0.001 //кДж/К*моль
 #define tau 0.001 // пикасекунды
 #define T 300 //K
-#define NSTEPS 500 //1000000
-#define STRIDE 10
+#define NSTEPS 10 //1000000
+#define STRIDE 1
 #define L 1.0
 
 
 void saveCoordinates(const std::string filename,
                      const std::string modifier,
-                    //  const std::vector<Atom>& atoms,
                      const std::vector<float3>& r,
                      const std::vector<float3>& v, 
                      int& N)
@@ -30,11 +29,9 @@ void saveCoordinates(const std::string filename,
     fprintf(fout, "DNA\n%d\n", N);
     for (int i = 0; i < N; i++)
     {
-        // printf(" mem %f", r[i].x);
         fprintf(fout, "%5d%-5s%5s%5d%8.3f%8.3f%8.3f%8.4f%8.4f%8.4f\n",
             i, "DNA",
             "K", i, 
-            // atoms[i].name.c_str(), atoms[i].index,
             r[i].x, r[i].y, r[i].z,
             v[i].x, v[i].y, v[i].z
         );
@@ -43,30 +40,10 @@ void saveCoordinates(const std::string filename,
     fclose(fout);
 }
 
-
-float3 f_i_new(int& i, int& N_pairs, std::vector<std::vector<int> >& k_for_i, HiCData& hic, int& max_U_ij, std::vector<float3>& r){
-    float3 V_cov;
-    float3 V_hic = {0, 0, 0};
-    float3 dist1 = r[i].distance(r[i-1]);
-    float3 dist2 = r[i+1].distance(r[i]);
-
-    V_cov = dist2 * k_c*2 - dist2/dist2.length() * 2 * k_c *r_0;
-    V_cov += dist1 * k_c*2 - dist1/ dist1.length() * 2 * k_c *r_0;
-    
-    float k_ij;
-    float r_ij_0;
-
-     for(int k = 0; k < k_for_i[i].size(); k++){
-        int num_of_pair = k_for_i[i][k];
-        int i_pair = hic.pairs[num_of_pair].i;
-        int j_pair = hic.pairs[num_of_pair].j;
-        int n_pair = hic.pairs[num_of_pair].n;
-        float3 dist0 = r[i_pair].distance(r[j_pair]);
-        k_ij = 50*n_pair/max_U_ij;
-        r_ij_0 = 0.19 + 0.01*max_U_ij/n_pair;
-        V_hic += dist0*k_ij * 2 - dist0/dist0.length()*r_ij_0;
-    }
-    return V_cov+V_hic;
+float3 pair_grad(HarmonicPair& pair, float3& r_i, float3& r_j){
+    float3 r_ij = r_i - r_j;
+    float3 grad_res = -(r_ij*2*pair.kc - r_ij/r_ij.length()*pair.kc*pair.r0);
+    return grad_res;
 }
 
 float3 v_i_new(float3 v_prev, float3 f){
@@ -83,8 +60,8 @@ float3 v_i_new(float3 v_prev, float3 f){
     float3 n_comp;
     float3 v;
 
-    n_comp = r_i_f*(2.0*k_b*T*lambda*m);
-    v = (f + v_prev*m/tau + v_prev*lambda*m/2 + n_comp)/(m/tau + lambda*m/2);
+    n_comp = r_i_f*std::sqrt(2.0*k_b*T*lambda*m/tau);
+    v = (f + v_prev*m/tau - v_prev*lambda*m/2 + n_comp)/(m/tau + lambda*m/2);
 
     return v;
 }
@@ -93,24 +70,40 @@ float3 r_i_new(float3 r_prev, float3 v_next){
     return v_next*tau + r_prev;
 }
 
-std::pair<std::vector<float3>, std::vector<float3> > step(HiCData& hic, std::vector<std::vector<int> >& k_for_i, std::vector<float3>& v, std::vector<float3>& r, int& max_U_ij ){
-    std::vector<float3> r_new;
-    std::vector<float3> v_new;
-    std::vector<float3> f_new;
+std::pair<std::vector<float3>, std::vector<float3> > step(std::vector<HarmonicPair> all_pairs, HiCData& hic, std::vector<float3>& v, std::vector<float3>& r, int& max_U_ij ){
+    std::vector<float3> r_new(hic.atomCount, {0.0f, 0.0f, 0.0f});
+    std::vector<float3> v_new(hic.atomCount, {0.0f, 0.0f, 0.0f});
+    std::vector<float3> f_new(hic.atomCount, {0.0f, 0.0f, 0.0f});
 
     int N = hic.atomCount;
     int N_pairs = hic.pairCount;
-    for(int i = 0; i < N; i++){
-        float3 res = f_i_new(i, N_pairs, k_for_i, hic, max_U_ij, r);
-        f_new.push_back(res);
+
+    float3 fi;
+    float3 fj;
+    HarmonicPair pair;
+    for(int p = 0; p < all_pairs.size(); p++){
+        pair = all_pairs[p];
+        printf("\n%d - pair -%d\n", pair.i, pair.j);
+        fi = pair_grad(pair, r[pair.i], r[pair.j]); //градиент пары
+        printf("\n Grad res %f %f %f \n", fi.x, fi.y, fi.z);
+        fj = -fi;
+        f_new[pair.i] += fi;
+        f_new[pair.j] += fj;
     }
+
     printf("\nCounted f");
+    float3 v_res;
     for (int i = 0; i < N; i++){
-        v_new.push_back(v_i_new(v[i], f_new[i]));
+        v_res = v_i_new(v[i], f_new[i]);
+        v_new.push_back(v_res);
+        printf("\n V res %f %f %f \n", v_res.x, v_res.y, v_res.z);
     }
     printf("\nCounted v");
+    float3 r_res;
     for (int i = 0; i < N; i++){
-        r_new.push_back(r_i_new(r[i], v_new[i]));
+        r_res = r_i_new(r[i], v_new[i]);
+        r_new.push_back(r_res);
+        printf("\n R res %f %f %f \n", r_res.x, r_res.y, r_res.z);
     }
     printf("\nCounted r\n");
     return std::make_pair(v_new, r_new);
@@ -122,8 +115,7 @@ std::vector<float3> counting(HiCData& hic){
     std::vector<float3> v_start(hic.atomCount, {0.0f, 0.0f, 0.0f});
 
     for(int i = 0; i < hic.atomCount; i++){
-        r_start.push_back(float3{1.0f*i, 0.0, 0.0});
-        // printf("kek %f\n", 0.1*i);
+        r_start.push_back(float3{1.0f*i - hic.atomCount/2, 0.0, 0.0});
     }
 
     int max_U_ij = 0;
@@ -134,21 +126,24 @@ std::vector<float3> counting(HiCData& hic){
     }
     
 
+    //harmonicPair (AllPairs)
+    std::vector<HarmonicPair> all_pairs;
+    HarmonicPair one_pair;
+    //adiing V_cov
+    for (int i = 0; i < hic.atomCount-1; i++){
+        one_pair.i = i;
+        one_pair.j = i+1;
+        one_pair.kc = k_c;
+        one_pair.r0 = r_0;
+        all_pairs.push_back(one_pair);
+    }
 
-    //numbers of pairs for i
-    std::vector<std::vector<int> > k_for_i;
-    std::vector<int> one_k_for_i;
-    for (int i = 0; i < hic.atomCount; i++){
-        one_k_for_i.clear();
-        for(int k = 0; k < hic.pairCount; k++){
-            int i_pair = hic.pairs[k].i;
-            int j_pair = hic.pairs[k].j;
-            int n_pair = hic.pairs[k].n;
-            if (i_pair == i || j_pair == i){
-                one_k_for_i.push_back(k);
-            }
-        }
-        k_for_i.push_back(one_k_for_i);
+    //adding V_hic
+    for (int p = 0; p < hic.pairCount; p++){
+        one_pair.i = hic.pairs[p].i;
+        one_pair.j = hic.pairs[p].j;
+        one_pair.kc = 50.0 * hic.pairs[p].n/max_U_ij;
+        one_pair.r0 = 0.19 + 0.01*max_U_ij/hic.pairs[p].n;
     }
 
 
@@ -166,7 +161,7 @@ std::vector<float3> counting(HiCData& hic){
             mod = "a";
         }
         temp = 0.0;
-        step_res = step(hic, k_for_i, v_new, r_new, max_U_ij);
+        step_res = step(all_pairs, hic, v_new, r_new, max_U_ij);
         v_new = step_res.first;
         r_new = step_res.second;
         // for(int i = 0; i < hic.atomCount; i++){
--- counter.h
+++ counter.h
@@ -3,15 +3,20 @@
 #include <string>
 
 
+struct HarmonicPair{
+    int i, j;
+    float kc, r0;
+};
+
 struct float3{
 	float x,y,z;
 
     float length() const {
         return std::sqrt(x*x + y*y + z*z);
     }
-    float3 distance(float3 other) {
-        return {x - other.x, y - other.y, z - other.z};
-    }
+    // float3 distance(float3 other) {
+    //     return {x - other.x, y - other.y, z - other.z};
+    // }
 
     float3 operator/(float scalar) const{
         return {x/scalar, y/scalar, z/scalar};
@@ -28,6 +33,9 @@ struct float3{
     float3 operator-(float3 other) const{
         return{x-other.x, y-other.y, z-other.z};
     }
+    float3 operator-() const {
+        return {-x, -y, -z};
+    }
 };
 
 void saveCoordinates(const std::string filename,
@@ -38,13 +46,12 @@ void saveCoordinates(const std::string filename,
                      int& N);
 
 
-
-float3 f_i_new(int& i, int& N_pairs, HiCData& hic, int& max_U_ij, std::vector<float3>& r);
+float3 pair_grad(HarmonicPair& pair, float3& r_i, float3& r_j);
 
 float3 v_i_new(float3 v_prev, float3 f);
 
 float3 r_i_new(float3 r_prev, float3 v_next);
 
-std::pair<std::vector<float3>, std::vector<float3> > step(int& N, std::vector<float3>& v, std::vector<float3>& r, std::vector<std::vector<int> >& U, int& max_U_ij );
+std::pair<std::vector<float3>, std::vector<float3> > step(std::vector<HarmonicPair> all_pairs, HiCData& hic, std::vector<float3>& v, std::vector<float3>& r, int& max_U_ij );
 
 std::vector<float3> counting(HiCData& hic);
